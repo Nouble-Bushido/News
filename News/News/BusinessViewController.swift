8749c7ea4b7fe00037fda0020342599b12b0da62
Artem <artchzhen@gmail.com>
2023-08-08 16:17:56 +0900

    init window

---
+++ News/News/BusinessViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BusinessViewController.swift
+//  News
+//
+//  Created by Артем Чжен on 08.08.2023.
+//
+
+import Foundation
---
+++ News/News/TabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarController.swift
+//  News
+//
+//  Created by Артем Чжен on 08.08.2023.
+//
+
+import Foundation
