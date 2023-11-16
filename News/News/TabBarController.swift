aa6529129c48c5c6260c8592975408a4c8002e02
Artem Chzhen <117162015+Nouble-Bushido@users.noreply.github.com>
2023-11-16 19:42:34 +0900

    refactored

--- News/News/TabBarController.swift
+++ News/News/TabBarController.swift
@@ -12,12 +12,42 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.tintColor = .black
         setupViewControllers()
+        setupTabBar()
     }
     
     private func setupViewControllers() {
         viewControllers = [
-            let navigationController = UINavigationController(rootViewController: <#T##UIViewController#>)
+            setupNavigationController(rootViewController: GeneralViewController(viewModel: GeneralViewModel()),
+                                      title: "General",
+                                      image: UIImage(systemName: "newspaper") ?? UIImage.add),
+            setupNavigationController(rootViewController: BusinessViewController(viewModel: BusinessViewModel()),
+                                      title: "Business",
+                                      image: UIImage(systemName: "briefcase") ?? UIImage.add),
+            setupNavigationController(rootViewController: TechnologyViewController(),
+                                      title: "Technology",
+                                      image: UIImage(systemName: "gyroscope") ?? UIImage.add)
         ]
+    }
+    
+    private func setupNavigationController(rootViewController: UIViewController,
+                                           title: String,
+                                           image: UIImage) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = image
+        rootViewController.navigationItem.title = title
+//        navigationController.navigationBar.prefersLargeTitles = true
+        
+        return navigationController
+    }
+    
+    private func setupTabBar() {
+        let apperance = UITabBarAppearance()
+        apperance.configureWithOpaqueBackground()
+        tabBar.scrollEdgeAppearance = apperance
+        
+        view.tintColor = .black
     }
 }
